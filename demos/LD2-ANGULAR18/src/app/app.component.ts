6eb90096d80a1c169eff502325f0cdf4add2be85
DEV <DEV@DEV.COM>
2025-07-01 17:02:39 +0530

    commit

--- demos/LD2-ANGULAR18/src/app/app.component.ts
+++ demos/LD2-ANGULAR18/src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Employee } from './model/Employee';
+import { EmployeeService } from './service/employee.service';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +10,15 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'LD2-ANGULAR18';
+  employeeList? : Employee[];
+  constructor(private employeeService: EmployeeService) {
+  }
+  ngOnInit(): void {
+      this.employeeService.getEmployees().subscribe((dataFromExternalService) => {
+          this.employeeList = dataFromExternalService;
+          console.log("Employees fetched successfully: ", this.employeeList);
+      });
+  }
 }
--- demos/LD2-ANGULAR18/src/app/service/employee.service.ts
+++ demos/LD2-ANGULAR18/src/app/service/employee.service.ts
@@ -1,7 +1,7 @@
 import { Inject } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import {Injectable} from "@angular/core";
-import { Employee } from "./model/Employee";
+import { Employee } from "../model/Employee";
     // The @Injectable decorator is used to mark the class as a service that can be injected into other components or services.
 @Injectable({
     // Sigleton Bean in Spring
@@ -9,7 +9,7 @@ import { Employee } from "./model/Employee";
 })
 // This is a custom service for managing employee data.
 // Whereas, the HttpClient is a built-in Angular service for making HTTP requests.
-class EmployeeService {
+export class EmployeeService {
     // TypeScript is a superset of JavaScript, so we can use TypeScript features like types and interfaces.
     // The baseURL is of type string and points to the backend API endpoint. 
     // Typescript ensures that the baseUrl is always a string and cannot be changed to a different type.
@@ -32,12 +32,11 @@ class EmployeeService {
 // The private keyword makes the httpClient property accessible only within the EmployeeService class.
 // The httpCient property is automatically initialized with the injected HttpClient instance.
     constructor(private httpClient: HttpClient){
-      
     }
     getEmployees() {
         console.log("Fetching employees from: " + this.baseUrl);
         // write the logic to fetch employees from the backend API endpoint.
-        
+    
         return this.httpClient.get<Employee[]>(this.baseUrl);
     }
 }
