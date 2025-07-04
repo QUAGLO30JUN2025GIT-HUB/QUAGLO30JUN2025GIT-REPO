ce50b8b3395b315364da7851acfdba5b19a28fa2
DEV <DEV@DEV.COM>
2025-07-04 12:01:55 +0530

    commit

--- demos/LD2-ANGULAR18/src/app-routing-module.ts
+++ demos/LD2-ANGULAR18/src/app-routing-module.ts
@@ -3,12 +3,15 @@ import {RouterModule,Routes} from '@angular/router';
 // import { LoginComponent } from './login/login.component';
 import { AddEmpComponent } from './app/add-emp/add-emp.component';
 import { ListEmpComponent } from './app/list-emp/list-emp.component';
+import { LoginComponent } from './app/login/login.component';
+
+// Please extract the routes and place it in the app.routes.ts file.
 const routes:Routes= [
    // Application routes can be defined here.
 //    { path:'login', component:LoginComponent},
-      {path:'employees', component:ListEmpComponent}
+      {path:'employees', component:ListEmpComponent},
 //    {path:'add', component:AddEmpComponent},
-//    {path:'**', component:LoginComponent} // Wildcard route for a 404 page
+   {path:'**', component:LoginComponent} // Wildcard route for a 404 page
 ]
 @NgModule({
     // The declarations array is used to declare the components, directives, and pipes that belong to this module.
--- demos/LD2-ANGULAR18/src/app/login/login.component.ts
+++ demos/LD2-ANGULAR18/src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -8,5 +10,42 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  // The ! operator is a non-null assertion operator in TypeScript.
+  // It tells the TypeScript compiler that the variable will be initialized later, so it can skip the strict null checks.
+  // This is useful when you are sure that the variable will be initialized before it is used
+  loginForm!: FormGroup;
+  submitted = false;
 
+  constructor(private formBuilder: FormBuilder, private router: Router) {
+    // The fromGroup is more declarative and allows you to define the structure of the form in a more readable way.
+    this.loginForm = this.formBuilder.group({
+      loginid: ['angular', [Validators.required, Validators.minLength(4)]],
+      password: ['ngpass', [Validators.required, Validators.minLength(6)]]
+      // loginid: ['angular'],
+      // password: ['ngpass']
+    });
+  }
+  onSubmit() {
+    this.submitted = true;
+    // The invalid property of the FormGroup is true if any of the controls in the group are invalid.
+    if (this.loginForm.invalid) {
+      console.log('Form Validation Failed');
+      return;
+    }
+    // The value property of the FormGroup returns an object containing the values of all the controls in the group.
+    const formData = this.loginForm.value;
+    // Hit the authservice to connect to the enpoint of your server and then authenticate the user in the backend. // JWT....... strored on the client browser.
+    // === checks for both value and type
+    if (formData.loginid === 'angular' || formData.password === 'ngpass') {
+      // Perform login action
+      console.log('Login successful');
+      sessionStorage.setItem('loginid', "yes");
+      this.router.navigate(['/empoyees']);
+    }
+    else {
+      // Handle login failure
+      console.log('Login failed');
+      sessionStorage.removeItem('loginid');
+    }
+  }
 }
